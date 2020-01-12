29248478741855a7161aa7c8a97cbad4afc9ba05
youhei sato <s.y.92.jr@gmail.com>
2020-01-13 00:45:39 +0900

    Lesson4 課題提出
    要素個数５のint型の配列arrayを宣言し特定の数値で初期化後、for文で値を前後に表示。

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+        
+        void Start () {
+                
+                int[] allay = {10, 20, 30, 40, 50};
+                
+                
+                for (int i = 0; i < allay.Length; i++) 
+                {
+                        Debug.Log (allay [i]);
+                }
+                for (int i = allay.Length -1; 0 <= i; i--) 
+                {
+                        Debug.Log (allay [i]);
+                }
+        }
+        
+        // Update is called once per frame
+        void Update () {
+        
+        }
+}
